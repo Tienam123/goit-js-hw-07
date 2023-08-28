8ee17ff7e0e434e4ac3144592fe6f40dc2d462e9
Vladyslav <dr.tienam123@gmail.com>
2023-08-29 02:17:34 +0300

    complete task #1

---
+++ js/task-01.js
@@ -0,0 +1,22 @@
+// Определяем ссылку на блок ul#categories
+const listEl = document.querySelector("#categories");
+
+// Задание №1 Определяем количество категорий и выводим в консоль
+const arrayFromList = listEl.children;
+console.log("Number of Categories: ", arrayFromList.length);
+
+// Задание 2 Получить доспуп к контенту каждого заголовка и вывести в консоль заголовок
+// и количество вложеных елементов
+
+const liEl = document.querySelectorAll(".item");
+//Перебираем полученную коллекцию с помощью метода forEach
+liEl.forEach((el) => {
+  // Определяем контент с помощью textContent
+  console.log("Categories: ", el.firstElementChild.textContent);
+  // Инициализируем коллекцию через следующего ребенка и получаем колекцию его детей
+  // Определяем длинну полученого массива
+  console.log(
+    "Elements: ",
+    el.firstElementChild.nextElementSibling.children.length
+  );
+});
