03f3b316a472ecb473d70f54671471d900e578c7
Vladyslav <dr.tienam123@gmail.com>
2023-09-01 23:48:10 +0300

    complete task #9(Added comments)

--- js/task-08.js
+++ js/task-08.js
@@ -9,23 +9,23 @@ refs.form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
     const user = {};
+    // Запрещаем обновление страницы
     event.preventDefault();
     const password = event.currentTarget.elements.password;
     const email = event.currentTarget.elements.email;
-    if (email.value == '') {
+    // Проверяем нет ли пустых полей в форме
+    if (email.value == "") {
         alert('Все поля должны быть заполнены');
         return;
-    } else if (password.value == '') {
+    } else if (password.value == "") {
         alert('Все поля должны быть заполнены');
         return;
+    //     Если нет записываем данные с полей в обьект
     } else {
             user.email = email.value;
             user.password = password.value;
     }
     console.log(user);
+    // Обнуляем поля
     event.currentTarget.reset();
-
-
-
-
 }
